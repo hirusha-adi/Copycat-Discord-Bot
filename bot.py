715d05c434cef89bd888247abfa15269f480c23e
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-07-14 12:46:20 +0530

    help command

--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
 import discord
+from datetime import datetime
 from discord.ext import commands
 import os
 
-client = commands.Bot(command_prefix='(')
+data = {
+    "title": "CopyCat Bot",
+    "repo": "https://github.com/hirusha-adi/Copycat-Discord-Bot",
+    "prefix": "rb"
+}
+
+client = commands.Bot(command_prefix=data['prefix'])
 
 
 def make_on():
@@ -27,12 +34,38 @@ def check_on_off():
 make_off()
 
 
-TOKEN = open("token.key").read()
-
-
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'We have logged in as {client.user} | {client.user.id}')
+
+
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(title=data['title'], url=data['repo'],
+                          description="Help for {title}".format(
+                              title=data['title']),
+                          timestamp=datetime.utcnow(),
+                          color=0x00FFFF)
+    embed.set_author(name=client.user.name,
+                     url=data['title'], icon_url=client.user.avatar_url)
+    embed.set_thumbnail(
+        url="https://cdn.discordapp.com/attachments/877796755234783273/997036774037655602/unknown.png")
+    embed.add_field(name=f"{data['prefix']}help",
+                    value="Display help", inline=False)
+    embed.add_field(name=f"{data['prefix']}start [@user]",
+                    value="Start repeating a user", inline=False)
+    embed.add_field(name=f"{data['prefix']}stop [@user]",
+                    value="Stop repeating a user", inline=False)
+    embed.add_field(name=f"About This Project",
+                    value="Made by Github user **@hirusha-adi**. This project is completely free and open source and under the MIT License", inline=False)
+    embed.set_footer(text=f'Requested by {ctx.author.name}')
+    await ctx.send(embed=embed)
+    await ctx.send()
+
+
+@client.command()
+async def start(ctx):
+    pass
 
 
 @client.event
@@ -42,16 +75,6 @@ async def on_message(message):
         return
 
     msg = message.content
-
-    if msg.startswith('(help'):
-        big_shit = f'''```{str(message.author)} requested Help!
-(help -> show this
-(pleasestartrepeating -> start
-(pleasestoprepeating -> stop
-(info -> information about the bot
-(status -> send the status```
-'''
-        await message.channel.send(big_shit)
 
     if msg.startswith('(pleasestartrepeating'):
         try:
@@ -80,14 +103,15 @@ async def on_message(message):
     else:
         try:
             if check_on_off() == "off":
-                # await message.channel.send("Please switch this on and try again")
                 pass
             elif check_on_off() == "on":
-                # + str(message.author.mention)
                 await message.channel.send(str(msg))
             else:
                 await message.channel.send("Something else is wrong - please ask ZeaCeR#5641 to check the code")
         except Exception as e:
             await message.channel.send(f'An error has occured: {e}')
 
-client.run(TOKEN)
+if __name__ == "__name__":
+    with open("token.key", "r", encoding='utf-8') as _token_file:
+        TOKEN = _token_file.read()
+    client.run(TOKEN)
